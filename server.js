9aa8181e44e5727c7e5e2381be509d3b3937bb52
kylest0kes <kylestokes1551@gmail.com>
2020-07-25 21:57:29 -0400

    heroku setup

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", {
   useNewUrlParser: true,
 });
 
